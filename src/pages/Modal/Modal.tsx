0b3402412de33ab9f3a1360e3ebf6e4cc6cab234
Gustavo Barzi <guhenriquebarzi@hotmail.com>
2022-06-02 09:36:12 -0300

    update: put modal deposit

--- src/pages/Modal/Modal.tsx
+++ src/pages/Modal/Modal.tsx
@@ -1,23 +1,58 @@
-import { Button } from "@mui/material";
+import * as React from 'react';
 import { Link } from 'react-router-dom';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Modal from '@mui/material/Modal';
+import Vinput from '../../shared/components/Vinput';
 
-export const Modal = () => {
+const style = {
+    position: 'absolute' as 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    borderradius: '10px',
+    border: '2px solid #000',
+    boxShadow: 24,
+    p: 4,
+  };
+
+export const DepositModal = () => {
+    const [open, setOpen] = React.useState(false);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
+
     return (
-        
+
         <div className="container__box ">
-                <div className="portifolio">
-                    <nav>
-                        <p className="nav__logo">GPB 0,0000</p>
-                        <ul className="nav__list">
-                            <li>
-                                <Link to="/deposit">
-                                    <Button variant="contained" color="success" classes={{ root: 'button' }}>Deposit</Button>
-                                </Link>
-                            </li>
-                        </ul>
-                    </nav>
-                </div>
+            <div className="portifolio">
+                <h2>Balance</h2>
+                <nav>
+                    <p className="nav__logo">GPB 0,0000</p>
+                    <ul className="nav__list">
+                        <li>
+                            <Link to="/portifolio">
+                                <Button onClick={handleOpen} variant="contained" color="success" classes={{ root: 'button' }}>Deposit</Button>
+                            </Link>
+                        </li>
+                    </ul>
+                </nav>
             </div>
+            <div>
+                <Modal
+                    open={open}
+                    onClose={handleClose}
+                    aria-labelledby="modal-modal-title"
+                    aria-describedby="modal-modal-description"
+                >
+                    <Box sx={style}>
+                        <Vinput />
+                        <Button color="success">Deposit</Button>
+                    </Box>
+                </Modal>
+            </div>
+        </div>
     );
 }
 
--- src/pages/Portifolio/Portifolio.tsx
+++ src/pages/Portifolio/Portifolio.tsx
@@ -1,12 +1,12 @@
+import { DepositModal } from '../Modal/Modal';
 import './portifolio.css';
-import AttachMoneyIcon from '@mui/icons-material/AttachMoney';
-import { Link } from 'react-router-dom';
-import { Button } from '@mui/material';
-
 const Portifolio = () => {
     return (
         <main className="container__port">
             <h1>Portifolio Overview</h1>
+            <div>
+                <DepositModal />
+            </div>
         </main>
     )
 }
