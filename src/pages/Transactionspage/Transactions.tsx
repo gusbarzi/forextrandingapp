c51eb7576bd7f5269d8ffdbef3acf7ee37493fc2
Gustavo Barzi <guhenriquebarzi@hotmail.com>
2022-06-10 16:03:33 -0300

    add: fixed bug

--- src/pages/Transactionspage/Transactions.tsx
+++ src/pages/Transactionspage/Transactions.tsx
@@ -4,12 +4,7 @@ import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import axios from 'axios';
 import Header from '../Homepage/Header/Header';
 import { UserContext } from '../../context/user';
-<<<<<<< HEAD
-import './transacs.css'
-=======
 import './transaction.css';
-
->>>>>>> 8341922034a74159a77994eddbb049e619b6b5ac
 
 interface IRequest {
   _id: string,
